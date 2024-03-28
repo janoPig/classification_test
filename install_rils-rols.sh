0034fbab805c87d4f322ac61e23d0dc0408bc372
Aleksandar <aleksandar.kartelj@gmail.com>
2024-03-28 18:52:19 +0100

    New results for RILS-ROLS.

--- install_rils-rols.sh
+++ install_rils-rols.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # install current release from pypi
-pip install rils-rols
+# pip install rils-rols
+
+# install local version for now
+pip install dist/rils-rols-1.6.4.tar.gz
 
 # build and install stable branch from sources
 # TODO: add this later...
