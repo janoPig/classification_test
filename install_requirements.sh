ffcd3e742a45b997abc412a58e72bebf046f5566
Jan Pigos <jan.pigos@gmail.com>
2024-04-04 11:25:16 +0200

    rr building

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
+sudo apt-get install build-essential -y
+sudo apt-get install python3-dev -y
+sudo apt install python3-pip -y
 # llvm 18
 wget https://apt.llvm.org/llvm.sh
 chmod u+x llvm.sh
