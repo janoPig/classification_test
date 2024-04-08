a66045dde3ab0804ab52ad5421298e9e77e3eb52
janoPig <jan.pigos@gmail.com>
2024-04-08 21:34:48 +0200

    Hyperparameters from DIGEN benchmark

--- code/methods_generators.py
+++ code/methods_generators.py
@@ -40,66 +40,118 @@ def cb_generator(trial : Trial, random_state):
 
 def gb_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "n_estimators": trial.suggest_int("n_estimators", 50, 500),
-        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
+        'loss': trial.suggest_categorical(name='loss', choices=['deviance', 'exponential']),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 1e-2, 1),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 200),
+        'max_depth': trial.suggest_int('max_depth', 1, 10),
+        'max_leaf_nodes': None,
+        'tol': 1e-7,
+        'n_iter_no_change': trial.suggest_int('n_iter_no_change', 1, 20),
+        'validation_fraction': trial.suggest_discrete_uniform('validation_fraction', 0.01, 0.31, 0.01)
     }
     return GradientBoostingClassifier(**params)
 
 def lgbm_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "verbose": -1,
-        "iterations": trial.suggest_int("iterations", 500, 5000, log=True),
+        'objective': 'binary',
+        'metric': 'binary_logloss',
+        'boosting_type': trial.suggest_categorical(name='boosting_type', choices=['gbdt', 'dart', 'goss']),
+        'num_leaves': trial.suggest_int('num_leaves', 2, 256),
+        'max_depth': trial.suggest_int('max_depth', 1, 10),
+        'n_estimators': trial.suggest_int('n_estimators', 10, 100),  # 200-6000 by 200
+        'deterministic': True,
+        'force_row_wise': True,
+        'njobs': 1,
     }
+    if 2 ** params['max_depth'] > params['num_leaves']:
+        params['num_leaves'] = 2 ** params['max_depth']
     return LGBMClassifier(**params)
 
 def xgb_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "iterations": trial.suggest_int("iterations", 500, 5000, log=True),
+        'booster': trial.suggest_categorical(name='booster', choices=['gbtree', 'dart']),
+        'n_estimators': trial.suggest_int('n_estimators', 10, 100),
+        'objective': 'binary:logistic',
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-5, 1e2),
+        'alpha': trial.suggest_loguniform('alpha', 1e-5, 1e2),
+        'gamma': trial.suggest_discrete_uniform('gamma', 0, 0.5, 0.1),
+        'eta': trial.suggest_loguniform('eta', 1e-8, 1),
+        'max_depth': trial.suggest_int('max_depth', 1, 10),
+        'eval_metric': 'logloss',
+        'tree_method': 'exact',
+        'nthread': 1,
+        'use_label_encoder': False,
     }
     return XGBClassifier(**params)
 
 def dt_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "criterion": trial.suggest_categorical('criterion', ['gini', 'entropy']),
-        "max_depth":trial.suggest_int('max_depth', 2, 32),
-        "min_samples_split": trial.suggest_int('min_samples_split', 2, 20),
-        "min_samples_leaf": trial.suggest_int('min_samples_leaf', 1, 20),
+        'criterion': trial.suggest_categorical('criterion', ['gini', 'entropy']),
+        'max_depth': trial.suggest_int('max_depth', 1, 10),
+        # 'max_depth_factor' : trial.suggest_discrete_uniform('max_depth_factor', 0, 2,0.1),
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 20),
+        'min_weight_fraction_leaf': 0.0,
+        'max_features': trial.suggest_categorical('max_features', [None, 'auto', 'log2']),
+        'max_leaf_nodes': None,
     }
     return DecisionTreeClassifier(**params)
 
 def lr_generator(trial : Trial, random_state):
-    params = {
-        "C": trial.suggest_float("C", 1e-6, 1e+6, log=True),
-    }
-    return LogisticRegression(**params)
+    params = {}
+    params['solver'] = trial.suggest_categorical(name='solver',
+                                                 choices=['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'])
+    params['dual'] = False
+    params['penalty'] = 'l2'
+    params['C'] = trial.suggest_loguniform('C', 1e-4, 1e4)
+    params['l1_ratio'] = None
+    if params['solver'] == 'liblinear':
+        params['penalty'] = trial.suggest_categorical(name='penalty', choices=['l1', 'l2'])
+        if params['penalty'] == 'l2':
+            params['dual'] = trial.suggest_categorical(name='dual', choices=[True, False])
+        else:
+            params['penalty'] = 'l1'
+
+    params['class_weight'] = trial.suggest_categorical(name='class_weight', choices=['balanced'])
+    param_grid = {'solver': params['solver'],
+                  'penalty': params['penalty'],
+                  'dual': params['dual'],
+                  'multi_class': 'auto',
+                  'l1_ratio': params['l1_ratio'],
+                  'C': params['C'],
+                  }
+    return LogisticRegression(**param_grid)
 
 def kn_generator(trial : Trial, random_state):
     params = {
-        "n_neighbors": trial.suggest_int("n_neighbors", 1, 50, log=True),
-        "leaf_size": trial.suggest_int("leaf_size", 1, 50),
-        "weights" : trial.suggest_categorical('weights', ['uniform', 'distance']),
-        "algorithm" : trial.suggest_categorical('algorithm', ['ball_tree', 'kd_tree', 'brute']),
-        "p": trial.suggest_float("p", 1.0, 5.0),
+        'n_neighbors': trial.suggest_int('n_neighbors', 1, 100),
+        'weights': trial.suggest_categorical('weights', ['uniform', 'distance']),
+        'p': trial.suggest_int('p', 1, 5),
+        'metric': trial.suggest_categorical('metric', ['euclidean', 'minkowski'])
     }
     return KNeighborsClassifier(**params)
 
 def rf_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "criterion": trial.suggest_categorical('criterion', ['gini', 'entropy', 'log_loss']),
-        "max_depth":trial.suggest_int('max_depth', 2, 32),
+        'n_estimators': trial.suggest_int('n_estimators', 10, 100),
+        'criterion': trial.suggest_categorical(name='criterion', choices=['gini', 'entropy']),
+        'max_depth': trial.suggest_int('max_depth', 1, 10),
+        'max_features': trial.suggest_categorical('max_features', [None, 'auto', 'log2']),
+        'bootstrap': True,
+        'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 20),
     }
     return RandomForestClassifier(**params)
 
 def svc_generator(trial : Trial, random_state):
     params = {
-        "random_state": random_state,
-        "C": trial.suggest_float("C", 1e-6, 1e+6, log=True),
-        "kernel": trial.suggest_categorical("kernel", ["linear", "poly", "rbf", "sigmoid"]),
-        "degree": trial.suggest_int("degree", 1, 8),
+        'kernel': trial.suggest_categorical(name='kernel', choices=['poly', 'rbf', 'linear', 'sigmoid']),
+        'C': trial.suggest_loguniform('C', 1e-2, 1e5),
+        'gamma': trial.suggest_categorical(name='gamma', choices=['scale', 'auto']),
+        'degree': trial.suggest_int('degree', 2, 5),
+        'class_weight': trial.suggest_categorical(name='class_weight', choices=[None, 'balanced']),
+        'coef0': trial.suggest_discrete_uniform('coef0', 0, 10, 0.1),
+        'tol': trial.suggest_loguniform('tol', 1e-5, 1e-2),
+        'probability': trial.suggest_categorical(name='probability', choices=[True]),
     }
     return SVC(**params)
